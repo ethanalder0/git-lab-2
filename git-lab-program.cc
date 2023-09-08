a0f8c8adac370fde52276997602a87fd92190811
Ethan Alder <143906858+ethanalder0@users.noreply.github.com>
2023-09-08 18:46:04 -0400

    Add files via upload
    program upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Ethan Alder>
+ *        Date: <9/8/23>
+ * Description: creating a program for github
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
